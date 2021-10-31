ae74a848cdb7a94256b309c1d4f1badcd2bdf7b5
Narmishka <73436375+Narmishka@users.noreply.github.com>
2021-10-31 19:02:46 +0100

    Update ftp.py

--- src/freebox_api/api/ftp.py
+++ src/freebox_api/api/ftp.py
@@ -1,30 +1,85 @@
-class Ftp:
+from freebox_api.access import Access
+
+class Lan:
     """
-    Ftp
+    LAN
     """
 
-    def __init__(self, access):
+    def __init__(self, access: Access):
         self._access = access
 
-    ftp_configuration_schema = {
-        "enabled": False,
-        "allow_anonymous": False,
-        "allow_anonymous_write": False,
-        "allow_remote_access": False,
-        "remote_domain": "",
-        "password": "",
-        "port_ctrl": 12345,
-        "port_data": 45678,
-    }
+    host_type = [
+        "workstation",
+        "laptop",
+        "smartphone",
+        "tablet",
+        "printer",
+        "vg_console",
+        "television",
+        "nas",
+        "ip_camera",
+        "ip_phone",
+        "freebox_player",
+        "freebox_hd",
+        "freebox_delta",
+        "networking_device",
+        "multimedia_device",
+        "freebox_mini",
+        "other",
+    ]
 
-    async def get_ftp_configuration(self):
-        """
-        Get ftp configuration
-        """
-        return await self._access.get("ftp/config/")
+    lan_host_data_schema = {"id": "", "primaryName": "", "hostType": host_type[0]}
 
-    async def set_ftp_configuration(self, ftp_configuration):
+    wol_schema = {"mac": "", "password": ""}
+
+    async def delete_lan_host(self, host_id, interface="pub"):
         """
-        Set ftp configuration
+        Delete lan host
         """
-        return await self._access.put("ftp/config/", ftp_configuration)
+        await self._access.delete(f"lan/browser/{interface}/{host_id}/")
+
+    async def get_config(self):
+        """
+        Get Lan configuration
+        """
+        return await self._access.get("lan/config/")
+
+    async def set_config(self, conf):
+        """
+        Update Lan config with conf dictionary
+        """
+        return await self._access.put("lan/config/", conf)
+
+    async def get_interfaces(self):
+        """
+        Get browsable Lan interfaces
+        """
+        return await self._access.get("lan/browser/interfaces")
+
+    async def get_hosts_list(self, interface="pub"):
+        """
+        Get the list of hosts on a given interface
+        """
+        return await self._access.get(f"lan/browser/{interface}")
+
+    async def get_host_information(self, host_id, interface="pub"):
+        """
+        Get specific host informations on a given interface
+        """
+        return await self._access.get(f"lan/browser/{interface}/{host_id}")
+
+    async def set_host_information(
+        self, host_id, lan_host_data=lan_host_data_schema, interface="pub"
+    ):
+        """
+        Update specific host informations on a given interface
+        """
+        return await self._access.put(
+            f"lan/browser/{interface}/{host_id}", lan_host_data
+        )
+
+    async def wake_lan_host(self, wol=wol_schema, interface="pub"):
+        """
+        Wake lan host
+        """
+        return await self._access.post(f"lan/wol/{interface}/", wol)
